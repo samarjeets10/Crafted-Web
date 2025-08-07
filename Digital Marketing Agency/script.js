9a2703664bb638317f54b37b2cca38a673cd4109
Samarjeet <samarsabale1021@gmail.com>
2025-08-07 22:05:35 +0530

    Add header section with image, title, and buttons; enhance styling for better layout

--- Digital Marketing Agency/script.js
+++ Digital Marketing Agency/script.js
@@ -14,3 +14,24 @@ navLinks.addEventListener('click', (e) => {
     navLinks.classList.remove("open");
     menuBtnIcon.setAttribute("class", "ri-menu-fill")
 });
+
+
+const scrollRevealOption = {
+    distance: "50px",
+    origin: "bottom",
+    duration: 1000,
+};
+
+ScrollReveal().reveal(".header_content h1", {
+    ...scrollRevealOption,
+})
+
+ScrollReveal().reveal(".header_content .section_discription", {
+    ...scrollRevealOption,
+    delay: 500,
+})
+
+ScrollReveal().reveal(".header_btn", {
+    ...scrollRevealOption,
+    delay: 1000,
+})
