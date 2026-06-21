3658617a027e6c181029be94bc05bc1f339b2921
Samarjeet <samarsabale1021@gmail.com>
2026-06-22 00:50:29 +0530

    feat: add Footer component to enhance site footer with navigation and subscription options.

--- Agency.AI/src/components/ContactUs.jsx
+++ Agency.AI/src/components/ContactUs.jsx
@@ -33,7 +33,7 @@ function ContactUs() {
     }
 
   return (
-    <div id='contac-us' className='flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 pt-30 text-gray-700 dark:text-white'>
+    <div id='contact-us' className='flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 pt-30 text-gray-700 dark:text-white'>
         <Title title='Reach out to us' desc='From strategy to execution, we craft digital solutions that move your business forward.' />
 
         <form onSubmit={onSubmit} className='grid sm:grid-cols-2 gap-3 sm:gap-5 max-w-2xl w-full'>
