1bb55a23c878535333f4fa5a669c384d26a73e55
Samarjeet <samarsabale1021@gmail.com>
2026-06-22 00:17:27 +0530

    feat: responsive contact form (name, email, message); submits via FormData component for user inquiries with form handling and toast notifications
    -with Web3Forms integration and toast notifications.

--- Agency.AI/src/App.jsx
+++ Agency.AI/src/App.jsx
@@ -5,6 +5,8 @@ import TrustedBy from './components/TrustedBy';
 import Services from './components/Services';
 import OurWork from './components/OurWork';
 import Teams from './components/Teams';
+import ContactUs from './components/ContactUs';
+import { Toaster } from 'react-hot-toast'
 
 function App() {
 
@@ -29,12 +31,14 @@ function App() {
 
   return (
     <div className='w-full min-h-screen dark:bg-black relative'>
+      <Toaster />
       <Navbar theme={theme} setTheme={setTheme}/>
       <Hero />
       <TrustedBy />
       <Services />
       <OurWork />
       <Teams />
+      <ContactUs />
     </div>
   )
 }
---
+++ Agency.AI/src/components/ContactUs.jsx
@@ -0,0 +1,76 @@
+import Title from './Title'
+import assets from '../assets/assets'
+import toast from 'react-hot-toast'
+
+function ContactUs() {
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+
+        const formData = new FormData(e.target);
+
+        formData.append("access_key", "a25e7298-f92d-43b9-88ea-c7bb0440874d");
+
+        try {
+            const response = await fetch("https://api.web3forms.com/submit", {
+                method: "POST",
+                body: formData
+            });
+            
+            const data = await response.json();
+            
+            if (data.success) {
+                toast.success('Thank you for your submission');
+                e.target.reset();
+            } else {
+                toast.error(data.message);
+            }
+
+        } catch (error) {
+            toast.error(error.message);
+        }
+
+    }
+
+  return (
+    <div id='contac-us' className='flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 pt-30 text-gray-700 dark:text-white'>
+        <Title title='Reach out to us' desc='From strategy to execution, we craft digital solutions that move your business forward.' />
+
+        <form onSubmit={onSubmit} className='grid sm:grid-cols-2 gap-3 sm:gap-5 max-w-2xl w-full'>
+            <div>
+                <p className='mb-2 text-sm font-medium'>Your Name</p>
+                <div className='flex pl-3 rounded-lg border border-gray-300 dark:border-gray-600'>
+                    <img src={assets.person_icon} alt="" />
+                    <input name='name' type="text" placeholder='Enter your name'
+                    className='w-full p-3 text-sm outline-none'
+                    required />
+                </div>
+            </div>
+
+            <div>
+                <p className='mb-2 text-sm font-medium'>Email id</p>
+                <div className='flex pl-3 rounded-lg border border-gray-300 dark:border-gray-600'>
+                    <img src={assets.email_icon} alt="" />
+                    <input name='email' type="email" placeholder='Enter your email'
+                    className='w-full p-3 text-sm outline-none'
+                    required />
+                </div>
+            </div>
+
+            <div className='sm:col-span-2'>
+                <p className='mb-2 text-sm font-medium'>Message</p>
+                <textarea name='message' rows={8} placeholder='Enter your message'
+                className='w-full p-3 text-sm outline-none rounded-lg border border-gray-300 dark:border-gray-600'
+                required />
+            </div>
+
+            <button type='submit' className='w-max flex gap-2 bg-primary text-white text-sm px-10 py-3 outline-none rounded-full cursor-pointer hover:scale-103 transition-all' >
+                Submit <img className='w-4' src={assets.arrow_icon} alt="" />
+            </button>
+
+        </form>
+    </div>
+  )
+}
+
+export default ContactUs
